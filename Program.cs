054b6f2440ce610241b416422f33212aff30443b
Thu Yein <thuyein1996alpha@gmail.com>
2025-05-01 14:10:33 +0700

    change to production

--- Program.cs
+++ Program.cs
@@ -17,11 +17,13 @@ builder.Services.AddDbContext<AppDbContext>(o => o.UseMySql(connectionString, Se
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
